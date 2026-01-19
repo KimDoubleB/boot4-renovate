f76a771fb793a5f48b7eff235c498e42a0883bab
KimDoubleB <tree9295@gmail.com>
2026-01-19 11:12:13 +0900

    refactor: add DynamicPropertyRegistrar for WireMock URLs
    Configure external service URLs dynamically using WireMock server ports

--- src/test/kotlin/com/kirndoubleb/labs/config/WireMockConfig.kt
+++ src/test/kotlin/com/kirndoubleb/labs/config/WireMockConfig.kt
@@ -4,6 +4,7 @@ import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.test.context.DynamicPropertyRegistrar
 
 @TestConfiguration(proxyBeanMethods = false)
 class WireMockConfig {
@@ -16,5 +17,16 @@ class WireMockConfig {
     @Bean(initMethod = "start", destroyMethod = "stop")
     fun notificationWireMockServer(): WireMockServer {
         return WireMockServer(wireMockConfig().dynamicPort())
+    }
+
+    @Bean
+    fun wireMockProperties(
+        paymentWireMockServer: WireMockServer,
+        notificationWireMockServer: WireMockServer
+    ): DynamicPropertyRegistrar {
+        return DynamicPropertyRegistrar { registry ->
+            registry.add("external.payment.url") { "http://localhost:${paymentWireMockServer.port()}" }
+            registry.add("external.notification.url") { "http://localhost:${notificationWireMockServer.port()}" }
+        }
     }
 }
